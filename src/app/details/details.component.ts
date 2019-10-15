699649909fb60b103cbf8fa8daafba716d0610c6
KADIR <kadir.korkmaz@gmail.com>
2019-10-15 18:23:47 +0300

    redirect if movie-details url requested on refresh

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { HttpService } from '../http.service';
 import { Movie } from './Movie';
 import { DataService } from "../data.service";
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
@@ -14,21 +16,20 @@ export class DetailsComponent implements OnInit {
 	private httpService:HttpService,
 	private data:DataService,
 	private myElement: ElementRef,
+	private router:Router,
   )
   { }
   
   movie:Movie = <Movie>{};
 	
   ngOnInit() { 
-	 
+	window.scroll(0,0); 
     this.getMovieDetails();
-	 
-	window.scroll(0,0);
-	 
   }
 
   getMovieDetails(): void {
 	let id = this.data.movieSource.getValue();
+	if(!id) this.router.navigate(['/']);
     this.httpService.getMovieDetails(id)
       .subscribe(movie => (this.movie = movie));
   }
